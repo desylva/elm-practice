5013e7cb10901dde3d9d24da65f318f88fe976af
Olivier de Sylva <olivier@mehode.com>
2017-06-21 23:42:51 +0100

    02-05

--- App.elm
+++ App.elm
@@ -3,35 +3,31 @@ module App exposing (..)
 import Html exposing (Html, button, div, text, program)
 import Html.Events exposing (onClick)
 
+
 -- MODEL
 
 type alias Model =
-  Bool
+  Int
 
 init : ( Model, Cmd Msg )
 init =
-  ( False, Cmd.none )
+  ( 0, Cmd.none )
 
 
 -- MESSAGES
 
 type Msg
-  = Expand
-  | Collapse
+  = Increment Int
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  if model then
-    div []
-        [ button [ onClick Collapse ] [ text "Collapse" ]
-        , text "Widget"
-        ]
-  else
-    div []
-        [ button [ onClick Expand ] [ text "Expand" ] ]
+  div []
+      [ button [ onClick (Increment 2) ] [ text "+" ]
+      , text (toString model)
+      ]
 
 
 -- UPDATE
@@ -39,10 +35,9 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Expand ->
-      ( True, Cmd.none )
-    Collapse ->
-      ( False, Cmd.none )
+    Increment howMuch ->
+      ( model + howMuch, Cmd.none )
+
 
 -- SUBSCRIPTIONS
 
